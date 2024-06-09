62e18e9ac28a88c6544da5e544d0f6147a5e531d
Hosting <163719152+hosting-everyone@users.noreply.github.com>
2024-06-09 17:20:23 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={client}>
         <Head>
-          <title>Demo Bot</title>
+          <title>CrocoBoco</title>
         </Head>
         {getLayout(<Component {...pageProps} />)}
       </QueryClientProvider>
